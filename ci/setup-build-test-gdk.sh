2744e6a3d348c209ba1d07305fe500b1b775d86d
Miron Zelina <mironzelina@improbable.io>
2021-01-29 16:46:45 +0000

    Refactor script to actually work

--- ci/setup-build-test-gdk.sh
+++ ci/setup-build-test-gdk.sh
@@ -7,27 +7,6 @@ fi
 
 source /opt/improbable/environment
 
-# Download the unreal-engine.version file from the GDK repo so we can run the example project builds on the same versions the GDK was run against.
-# This is not the pinnacle of engineering, as we rely on GitHub's web interface to download the file, but it seems like GitHub disallows git archive
-# which would be our other option for downloading a single file.
-# Also resolve the GDK branch to run against. The order of priority is:
-# TEST_REPO_BRANCH envvar > same-name branch as the branch we are currently on > UnrealGDKVersion.txt > "master".
-GDK_BRANCH_LOCAL="${TEST_REPO_BRANCH:-}"
-if [ -z "${GDK_BRANCH_LOCAL}" ]; then
-    GDK_REPO_HEADS=$(git ls-remote --heads "git@github.com:spatialos/UnrealGDK.git" "${BUILDKITE_BRANCH}")
-    EXAMPLEPROJECT_REPO_HEAD="refs/heads/${BUILDKITE_BRANCH}"
-    if echo "${GDK_REPO_HEADS}" | grep -qF "${EXAMPLEPROJECT_REPO_HEAD}"; then
-        GDK_BRANCH_LOCAL="${BUILDKITE_BRANCH}"
-    else
-        GDK_VERSION=$(cat UnrealGDKVersion.txt)
-        if [ -z "${GDK_VERSION}" ]; then
-            GDK_BRANCH_LOCAL="master"
-        else
-            GDK_BRANCH_LOCAL="${GDK_VERSION}"
-        fi
-    fi
-fi
-
 pushd "$(dirname "$0")"
     GDK_HOME="${1:-"$(pwd)/.."}"
     GCS_PUBLISH_BUCKET="${2:-io-internal-infra-unreal-artifacts-production/UnrealEngine}"
@@ -38,7 +17,28 @@ pushd "$(dirname "$0")"
     TEST_REPO_URL="git@github.com:spatialos/UnrealGDKTestGyms.git"
     TEST_REPO_MAP="EmptyGym"
     TEST_PROJECT_NAME="GDKTestGyms"
-    CHOSEN_TEST_REPO_BRANCH="${GDK_BRANCH_LOCAL}"
+
+    # Resolve the TestGym branch to run against. The order of priority is:
+    # TEST_REPO_BRANCH envvar > same-name branch as the branch we are currently on > UnrealGDKTestGymsVersion.txt > "master".
+    TEST_REPO_BRANCH_LOCAL="${TEST_REPO_BRANCH:-}"
+    if [ -z "${TEST_REPO_BRANCH_LOCAL}" ]; then
+        TEST_REPO_HEADS=$(git ls-remote --heads "${TEST_REPO_URL}" "${BUILDKITE_BRANCH}")
+        GDK_REPO_HEAD="refs/heads/${BUILDKITE_BRANCH}"
+        if echo "${TEST_REPO_HEADS}" | grep -qF "${GDK_REPO_HEAD}"; then
+            TEST_REPO_BRANCH_LOCAL="${BUILDKITE_BRANCH}"
+        else
+            # This is a slight hack, we rely on the fact that this version should work even if it's not the TestGym repo
+            # (also currently this script only operates on the TestGym repo)
+            TESTGYM_VERSION=$(cat UnrealGDKTestGymsVersion.txt)
+            if [ -z "${TESTGYM_VERSION}" ]; then
+                TEST_REPO_BRANCH_LOCAL="master"
+            else
+                TEST_REPO_BRANCH_LOCAL="${TESTGYM_VERSION}"
+            fi
+        fi
+    fi
+
+    CHOSEN_TEST_REPO_BRANCH="${TEST_REPO_BRANCH_LOCAL}"
 
     # Download Unreal Engine
     echo "--- get-unreal-engine"
