9147f82d435964f9be77b3f564dbe7743989e37f
Junting Yu <kenyu@improbable.io>
2021-02-01 09:49:08 +0800

    turn logs for heartbeat timer investigating

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetConnection.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetConnection.cpp
@@ -42,11 +42,6 @@ void USpatialNetConnection::CleanUp()
 {
 	UE_LOG(LogSpatialNetConnection, Log, TEXT("USpatialNetConnection CleanUp PlayerController %s entity %lld"),
 		   *AActor::GetDebugName(PlayerController), PlayerControllerEntity);
-	if (HeartbeatTimer.IsValid())
-	{
-		UE_LOG(LogSpatialNetConnection, Fatal, TEXT("CleanUp before timer disabled"), *AActor::GetDebugName(PlayerController),
-			   PlayerControllerEntity);
-	}
 	if (USpatialNetDriver* SpatialNetDriver = Cast<USpatialNetDriver>(Driver))
 	{
 		SpatialNetDriver->ClientConnectionManager->CleanUpClientConnection(this);
